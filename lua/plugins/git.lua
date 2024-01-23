b3ec89ae751d16dbfcffb0319cb05161afa94b2f
flexagoon <code@fxgn.dev>
2024-01-23 22:02:23 +0300

    feat: configure git integration

---
+++ lua/plugins/git.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      kind = "auto",
+      mappings = {
+        popup = {
+          ["p"] = "PushPopup",
+          ["P"] = "PullPopup",
+        },
+      },
+      integrations = {
+        telescope = true,
+      },
+    },
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<CR>" },
+      { "<leader>gc", "<cmd>Neogit commit<CR>" },
+      { "<leader>gp", "<cmd>Neogit push<CR>" },
+    }
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      on_attach = function()
+        local gs = package.loaded.gitsigns
+
+        vim.keymap.set("n", "<leader>gs", gs.stage_hunk)
+        vim.keymap.set("n", "<leader>gS", gs.stage_buffer)
+        vim.keymap.set("n", "<leader>gu", gs.undo_stage_hunk)
+        vim.keymap.set("n", "<leader>gh", gs.preview_hunk)
+        vim.keymap.set("v", "<leader>gs", function()
+          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end)
+      end
+    }
+  },
+}
