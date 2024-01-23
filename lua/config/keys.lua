1472b4999ec2ae5aac67700ca5979fa3f44e8c36
flexagoon <code@fxgn.dev>
2024-01-23 22:33:14 +0300

    keys: add doom emacs-style bindings for exiting

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -2,4 +2,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = vim.g.mapleader
 
-vim.keymap.set("n", "<leader>s", ":w<CR>")
+-- Doom Emacs-like bindings for saving and exiting
+vim.keymap.set("n", "<leader>s", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
+vim.keymap.set("n", "<leader>qq", "<cmd>q!<CR>")
