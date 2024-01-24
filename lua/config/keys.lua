3d564519923d59918043c43e2bc593afb6e6ed19
flexagoon <code@fxgn.dev>
2024-01-24 10:55:08 +0300

    keys: add a keybind for alternative buffer

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -7,3 +7,6 @@ vim.keymap.set("n", "<leader>s", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>wq", "ZZ")
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
 vim.keymap.set("n", "<leader>Q", "ZQ")
+
+-- Switch to alternative buffer
+vim.keymap.set("n", "<leader>`", "<cmd>b#<CR>")
