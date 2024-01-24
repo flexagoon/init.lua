a2767433430b9ea568ecc55fbd41a44aa35f8ba1
flexagoon <code@fxgn.dev>
2024-01-24 10:52:50 +0300

    keys: update quitting keybinds

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -4,5 +4,6 @@ vim.g.maplocalleader = vim.g.mapleader
 
 -- Doom Emacs-like bindings for saving and exiting
 vim.keymap.set("n", "<leader>s", "<cmd>w<CR>")
-vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
-vim.keymap.set("n", "<leader>qq", "<cmd>q!<CR>")
+vim.keymap.set("n", "<leader>wq", "ZZ")
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
+vim.keymap.set("n", "<leader>Q", "ZQ")
