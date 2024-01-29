ca666933749b8d80bd91be9552448dfccc805cd6
flexagoon <code@fxgn.dev>
2024-01-29 19:32:13 +0300

    telescope: add icons

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons"
+    },
     opts = function()
       local actions = require("telescope.actions")
       return {
