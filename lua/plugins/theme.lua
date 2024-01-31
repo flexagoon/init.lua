15471ee973efd0f4e12316a2ab6d0778aa20d8e4
flexagoon <code@fxgn.dev>
2024-02-01 01:37:30 +0300

    theme: switch to gruvbox-material

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,12 @@
 return {
-  "catppuccin/nvim",
+  "sainnhe/gruvbox-material",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("catppuccin-frappe")
+    vim.opt.termguicolors = true
+
+    vim.g.gruvbox_material_better_performance = 1
+    vim.g.gruvbox_material_enable_italic = 1
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.cmd.colorscheme("gruvbox-material")
   end
 }
