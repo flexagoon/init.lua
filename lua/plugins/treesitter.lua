822a2c26ee7b4d5f688f2ad945bfbe263ef1b438
flexagoon <code@fxgn.dev>
2024-02-01 01:37:45 +0300

    treesitter: enable smart folding

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+      -- Enable folding
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+      vim.opt.foldlevel = 99
+
       require("nvim-treesitter.configs").setup({
         auto_install = true,
 
