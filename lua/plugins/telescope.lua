a22aecd5e5ad1bfda779e97d476db56629ae662d
flexagoon <code@fxgn.dev>
2024-02-04 21:27:46 +0300

    telescope: use native fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,17 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons"
+      "nvim-tree/nvim-web-devicons",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make"
+      }
     },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("fzf")
+    end,
     opts = function()
       local actions = require("telescope.actions")
       local state = require("telescope.actions.state")
