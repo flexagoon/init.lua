3a0ffbdc7b684cc856a3091544f3becf0ab19302
flexagoon <code@fxgn.dev>
2024-03-08 13:23:22 +0300

    todo-comments: install

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,5 +18,7 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = "markdown",
     build = function() vim.fn["mkdp#util#install"]() end,
-  }
+  },
+
+  { "folke/todo-comments.nvim", config = true },
 }
