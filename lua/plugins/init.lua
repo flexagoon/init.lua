7dc79a175641773852bc860e9f47f9a797641bc6
flexagoon <code@fxgn.dev>
2024-04-09 12:49:14 +0300

    comment.nvim: use built-in nvim commenting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,12 +2,6 @@ return {
   -- Automatic indentation
   "tpope/vim-sleuth",
 
-  -- Quickly comment lines
-  {
-    "numToStr/Comment.nvim",
-    config = true,
-  },
-
   -- Track my activity with ActivityWatch
   "ActivityWatch/aw-watcher-vim",
 
