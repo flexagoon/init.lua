4675c8ec9ee0ccf57f5ce64e408d25ff25c50f4f
flexagoon <code@fxgn.dev>
2024-04-13 15:11:00 +0300

    theme: switch to kanagawa

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,7 @@
 return {
-  "sainnhe/gruvbox-material",
+  "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
-    vim.opt.termguicolors = true
-
-    vim.g.gruvbox_material_better_performance = 1
-    vim.g.gruvbox_material_enable_italic = 1
-    vim.g.gruvbox_material_enable_bold = 1
-    vim.cmd.colorscheme("gruvbox-material")
+    vim.cmd.colorscheme("kanagawa")
   end,
 }
