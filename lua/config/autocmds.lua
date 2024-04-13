3bdeefec32696dcd4145b8aebb9ef3b503acd31f
flexagoon <code@fxgn.dev>
2024-04-13 15:30:31 +0300

    autocmds: trigger help file autocmd on BufWinEnter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,8 @@
 -- Open help in vertical split if there is enough space
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd("BufWinEnter", {
   callback = function()
     if vim.bo.buftype == "help" and vim.o.columns / 2 >= 80 then
       vim.cmd("wincmd L")
     end
-  end
+  end,
 })
