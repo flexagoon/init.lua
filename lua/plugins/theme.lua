657ea4ab92a9528744ffa6579892bff3b4d09470
flexagoon <code@fxgn.dev>
2024-05-11 06:42:49 +0800

    theme: dim inactive buffers

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,6 +2,10 @@ return {
   "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
+    require("kanagawa").setup({
+      compile = true,
+      dimInactive = true,
+    })
     vim.cmd.colorscheme("kanagawa")
   end,
 }
