7235bd1637aa577faa61dcecd81cb11518af65d2
flexagoon <code@fxgn.dev>
2024-09-10 19:19:06 +0300

    chezmoi: highlight template files without a plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,21 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     end
   end,
 })
+
+-- Correct syntax highlighting in chezmoi template files
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = vim.fn.expand("~") .. "/.local/share/chezmoi/*.tmpl",
+  callback = function()
+    local filename = vim.fn.expand("%:p")
+    local original_filename =
+        filename
+        :gsub("%.local/share/chezmoi/", "")
+        :gsub("/dot_", ".")
+        :gsub("%.tmpl$", "")
+    local filetype = vim.filetype.match({
+      buf = 0,
+      filename = original_filename,
+    })
+    vim.bo.filetype = filetype
+  end,
+})
