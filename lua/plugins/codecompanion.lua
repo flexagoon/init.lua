e30aa01eee96202e080a468e2c722be99ced15be
flexagoon <code@fxgn.dev>
2025-07-28 18:10:24 +0300

    codecompanion: install codecompanion

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,28 @@
+require("codecompanion").setup({
+  strategies = {
+    chat = {
+      adapter = "gemini",
+      keymaps = {
+        completion = {
+          modes = {
+            i = "<C-n>",
+          },
+        },
+      },
+    },
+    inline = {
+      adapter = "gemini",
+    },
+  },
+  adapters = {
+    gemini = function()
+      return require("codecompanion.adapters").extend("gemini", {
+        env = {
+          api_key = "cmd:rbw get Google -f \"Gemini API\"",
+        },
+      })
+    end,
+  },
+})
+
+vim.keymap.set("n", "<leader>a", "<cmd>CodeCompanionChat Toggle<CR>")
