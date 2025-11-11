9c14f6bd7cebef24145af0d3f76c39f0a1fa148f
flexagoon <code@fxgn.dev>
2025-11-11 02:16:02 +0100

    lsp: add capability guard to formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,25 +29,29 @@ local ignored_formatters = {
 local lsp_formatting_group = vim.api.nvim_create_augroup("LspFormatting", {})
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
     local opts = { buffer = ev.buf }
 
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, opts)
 
-    vim.api.nvim_clear_autocmds({ group = lsp_formatting_group, buffer = ev.buf })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = "LspFormatting",
-      buffer = ev.buf,
-      callback = function()
-        vim.lsp.buf.format({
-          async = true,
-          filter = function(client)
-            return not vim.tbl_contains(ignored_formatters, client.name)
-          end,
-        })
-      end,
-    })
+    if not client:supports_method("textDocument/willSaveWaitUntil")
+        and client:supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = lsp_formatting_group, buffer = ev.buf })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = lsp_formatting_group,
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({
+            async = true,
+            filter = function(f_client)
+              return not vim.tbl_contains(ignored_formatters, f_client.name)
+            end,
+          })
+        end,
+      })
+    end
   end,
 })
 
