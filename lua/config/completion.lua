4e3294e09262d5b00a3fb9ee094be35e8acdfd49
flexagoon <code@fxgn.dev>
2025-11-11 02:07:39 +0100

    completion: simplify completion implementation

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -1,67 +1,32 @@
 vim.o.completeopt = "menuone,noinsert,popup,fuzzy"
+vim.o.complete = "o"
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
-    vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
-      autotrigger = false,
-      convert = function(item)
-        local kind = vim.lsp.protocol.CompletionItemKind[item.kind] or ""
-        return {
-          menu = "",
-          kind_hlgroup = "CmpItemKind" .. kind,
-        }
-      end,
-    })
-  end,
-})
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    if client:supports_method("textDocument/completion") then
+      local trigger_chars = client.server_capabilities.completionProvider.triggerCharacters or {}
+      for i = 32, 255 do
+        local char = string.char(i)
+        if vim.fn.match(char, "[[:keyword:]]") >= 0 then
+          table.insert(trigger_chars, char)
+        end
+      end
+      client.server_capabilities.completionProvider.triggerCharacters = trigger_chars
 
-local COMPLETION_DELAY_MS = 25
-
-local completion_timer = vim.uv.new_timer()
-
-local function should_trigger_completion(char)
-  local is_keyword = vim.fn.match(char, "[[:keyword:]]") >= 0
-  local popup_closed = vim.fn.pumvisible() == 0
-  return is_keyword and popup_closed
-end
-
-local function is_lsp_trigger(char)
-  for _, client in ipairs(vim.lsp.get_clients()) do
-    local completion_provider = client.server_capabilities.completionProvider
-
-    if completion_provider then
-      local triggers = completion_provider.triggerCharacters or {}
-      if vim.tbl_contains(triggers, char) then return true end
-    end
-  end
-
-  return false
-end
-
-vim.api.nvim_create_autocmd("InsertCharPre", {
-  callback = function()
-    ---@diagnostic disable-next-line: need-check-nil
-    completion_timer:stop()
-
-    local char = vim.v.char
-    if should_trigger_completion(char) or is_lsp_trigger(char) then
-      ---@diagnostic disable-next-line: need-check-nil
-      completion_timer:start(
-        COMPLETION_DELAY_MS,
-        0,
-        vim.schedule_wrap(vim.lsp.completion.get)
-      )
+      vim.lsp.completion.enable(true, ev.data.client_id, ev.buf, {
+        autotrigger = true,
+        convert = function(item)
+          local kind = vim.lsp.protocol.CompletionItemKind[item.kind] or ""
+          return {
+            menu = "",
+            kind_hlgroup = "CmpItemKind" .. kind,
+          }
+        end,
+      })
     end
   end,
 })
-
-vim.keymap.set("i", "<C-n>", function()
-  if vim.fn.pumvisible() ~= 0 then
-    vim.api.nvim_feedkeys("", "n", true)
-  else
-    vim.lsp.completion.get()
-  end
-end)
 
 vim.keymap.set("i", "<CR>", function()
   if vim.fn.pumvisible() ~= 0 then
